6d3d7eb82d52c0d6f851436711c943e871c4f385
tinkov <vasy499@mail.ru>
2022-12-12 16:43:49 +0300

    Initial commit

---
+++ src/ExceptionPassword/WrongLoginException.java
@@ -0,0 +1,10 @@
+package ExceptionPassword;
+
+public class WrongLoginException extends Exception{
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
